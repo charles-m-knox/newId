68a25a49810a5b10c4538559ae00039b531f8eb4
charles-m-knox <8022749+charles-m-knox@users.noreply.github.com>
2014-09-16 10:32:31 -0700

    Code Refactorization/Dependency Removal
    Removed dependency on System. Used Jagged arrays instead of multiple
    objects. Code is less than 100 lines.

--- newIdGeneratorAddin/newIdGen.cs
+++ newIdGeneratorAddin/newIdGen.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using ExcelDna.Integration;
+﻿using ExcelDna.Integration;
 
 namespace newIdGeneratorAddin
 {
@@ -7,11 +6,10 @@ namespace newIdGeneratorAddin
     {
         public void AutoOpen() { }
         public void AutoClose() { }
-        
 
         [ExcelFunction(Description = "Converts a 15-digit ID into an 18-digit ID based on the Salesforce.com algorithm.",
                 Category = "VLOOKUP Case Insensitivity")]
-        public static string newId([ExcelArgument(Description="15-digit ID that is ONLY 0-9 and A-Z to be converted to 18 digit unique ID.", Name="originalId")]string inputValue)
+        public static string newId([ExcelArgument(Description = "15-digit ID that is ONLY 0-9 and A-Z to be converted to 18 digit unique ID.", Name = "originalId")]string inputValue)
         {
             try
             {
@@ -19,117 +17,73 @@ namespace newIdGeneratorAddin
                 //Output:   18-digit alphanumeric string whose last 3 are based on the first 3 groups of 5 characters
 
                 //Step 1: Split inputValue into three sections
-                char[] charGroup1 = inputValue.ToCharArray(0, 5);
-                char[] charGroup2 = inputValue.ToCharArray(5, 5);
-                char[] charGroup3 = inputValue.ToCharArray(10, 5);
-
-                //Step 2: Reverse the string the object-oriented way
-                char[] revGroup1 = new char[5];
-                char[] revGroup2 = new char[5];
-                char[] revGroup3 = new char[5];
-                for (int i = 0; i < 5; i++)
+                char[][] charGroups = new char[3][];
+                for (int i = 0; i < 3; i++)
                 {
-                    revGroup1[i] = charGroup1[4 - i];
-                    revGroup2[i] = charGroup2[4 - i];
-                    revGroup3[i] = charGroup3[4 - i];
+                    charGroups[i] = inputValue.ToCharArray(5 * i, 5);
                 }
 
+                //Step 2: Reverse the string the object-oriented way
+                char[][] reversedChars = new char[3][];
+                for (int i = 0; i < 3; i++)
+                {
+                    reversedChars[i] = new char[5];
+                    for (int j = 0; j < 5; j++)
+                    {
+                        reversedChars[i][j] = charGroups[i][4 - j];
+                    }
+                }
 
                 //Step 3/4: Lazy Lookup table.
                 char[] algorithmAlphabet = new char[]{
 	            'A','B','C','D','E','F','G','H','I','J','K','L',
                 'M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
-                '0','1','2','3','4','5','6','7','8','9'
-            };
+                '0','1','2','3','4','5','6','7','8','9' };
+
                 string[] algorithmBinary = new string[]{
 	            "00000","00001","00010","00011","00100","00101","00110","00111","01000","01001","01010","01011",
                 "01100","01101","01110","01111","10000","10001","10010","10011","10100","10101","10110","10111",
-                "11000","11001","11010","11011","11100","11101","11110","11111"
-            };
+                "11000","11001","11010","11011","11100","11101","11110","11111" };
+
                 //Step 3: If the character is an Uppercase A-Z, then set it to 1. Otherwise, 0
-                //revGroup1:
-                for (int j = 0; j < 5; j++)
+                char[][] binaryChars = new char[3][];
+                for (int k = 0; k < 3; k++)
                 {
-                    bool flag = true;
-                    for (int i = 0; i < 26; i++)
+                    binaryChars[k] = new char[5];
+                    for (int j = 0; j < 5; j++)
                     {
-                        if (revGroup1[j] == algorithmAlphabet[i])
+                        bool flag = true;
+                        for (int i = 0; i < 26; i++)
                         {
-                            flag = false;
+                            if (reversedChars[k][j] == algorithmAlphabet[i])
+                            {
+                                flag = false;
+                            }
                         }
-                    }
-                    if (flag)
-                    {
-                        revGroup1[j] = '0';
-                    }
-                    else
-                    {
-                        revGroup1[j] = '1';
-                    }
-                }
-                //revGroup2:
-                for (int j = 0; j < 5; j++)
-                {
-                    bool flag = true;
-                    for (int i = 0; i < 26; i++)
-                    {
-                        if (revGroup2[j] == algorithmAlphabet[i])
-                        {
-                            flag = false;
-                        }
-                    }
-                    if (flag)
-                    {
-                        revGroup2[j] = '0';
-                    }
-                    else
-                    {
-                        revGroup2[j] = '1';
-                    }
-                }
-                //revGroup3:
-                for (int j = 0; j < 5; j++)
-                {
-                    bool flag = true;
-                    for (int i = 0; i < 26; i++)
-                    {
-                        if (revGroup3[j] == algorithmAlphabet[i])
-                        {
-                            flag = false;
-                        }
-                    }
-                    if (flag)
-                    {
-                        revGroup3[j] = '0';
-                    }
-                    else
-                    {
-                        revGroup3[j] = '1';
-                    }
-                }
-                //Step 4: Do the replacements.
-                //00000 = A, 00001 = B, 00010 = C, etc, until 11110 = 4, 11111 = 5
-                string outputValue1 = new string(revGroup1), outputValue2 = new string(revGroup2), outputValue3 = new string(revGroup3);
-
-                for (int i = 0; i < 32; i++)
-                {
-                    if (outputValue1 == algorithmBinary[i])
-                    {
-                        outputValue1 = algorithmAlphabet[i].ToString();
-                    }
-                    if (outputValue2 == algorithmBinary[i])
-                    {
-                        outputValue2 = algorithmAlphabet[i].ToString();
-                    }
-                    if (outputValue3 == algorithmBinary[i])
-                    {
-                        outputValue3 = algorithmAlphabet[i].ToString();
+                        binaryChars[k][j] = (flag) ? '0' : '1';
                     }
                 }
 
-                string outputValue = string.Concat(inputValue, outputValue1, outputValue2, outputValue3);
+                //Step 4: Do the binary replacements.
+                string[] outputValues = new string[3];
+                string returnValue = inputValue;
+                for (int i = 0; i < 3; i++)
+                {
+                    outputValues[i] = new string(binaryChars[i]);
+                }
 
-                return outputValue;
+                for (int k = 0; k < 3; k++)
+                {
+                    for (int i = 0; i < 32; i++)
+                    {
+                        if (outputValues[k] == algorithmBinary[i])
+                        {
+                            outputValues[k] = algorithmAlphabet[i].ToString();
+                        }
+                    }
+                    returnValue += outputValues[k];
+                }
+                return returnValue;
             }
             catch
             {
